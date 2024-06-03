d060c24a1b1c06a45e4b8e779b92bb3d1bfa7d01
Zekrom <maldonado.luis.iqm@gmail.com>
2024-06-03 15:52:04 -0600

    Se añada página de contacto así como su archivo js

--- javascript/contact.js
+++ javascript/contact.js
@@ -12,47 +12,57 @@
       const emailError = document.getElementById("emailError");
       const messageError = document.getElementById("messageError");
 
-      // Inicializar bandera de validación
+      // Mensaje de enviado
+      const alertMessage = document.getElementById("alertMessage");
+
+      // Inicializar validación
       let Isvalid = true;
 
-      // Resetear mensajes de error
+      // Resetear mensajes de error cuando esté correcto
       nameError.textContent = "";
       phoneError.textContent = "";
       emailError.textContent = "";
       messageError.textContent = "";
 
       // Validar nombre
-      const namePattern = /^[a-zA-Z\s]+$/;
-      if (!namePattern.test(name)) {
-        nameError.textContent = "El nombre debe contener solo letras y espacios.";
+      const patronNombre = /^[a-zA-Z\s]+$/; // ^indica que debe iniciar con lo que tiene a continuación
+      if (!patronNombre.test(name)) {
+        nameError.textContent = "Coloca un nombre válido.";
         Isvalid = false;
       }
 
       // Validar teléfono (solo números)
-      const phonePattern = /^[0-9]{10}$/; //{10} significa que solo pueden ser 10 números
-      if (!phonePattern.test(phone)) {
-        phoneError.textContent = "Ingresa tu número a 10 dígitos.";
+      const numCel = /^[0-9]{10}$/; //{10} significa que solo pueden ser 10 números
+      if (!numCel.test(phone)) {
+        phoneError.textContent = "Ingresa tu número celular.";
         Isvalid = false;
       }
 
       // Validar correo electrónico
-      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-      if (!emailPattern.test(email)) {
-        emailError.textContent = "Ingresa un correo electrónico válido.";
+      const patronCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+      if (!patronCorreo.test(email)) {
+        emailError.textContent = "Ingresa un correo electrónico.";
         Isvalid = false;
       }
 
       // Validar mensaje
       if (message.trim() === "") {
-        messageError.textContent = "El mensaje no puede estar vacío.";
+        messageError.textContent = "Mensaje Inválido.";
         Isvalid = false;
       }
 
-      // Retornar el estado de validación
+      // Retornar validación
       return Isvalid;
-    }
-/* 
-    function validateAndSendEmail(event) {
+    }// función validateform
+
+    function limpiarFormulario(){
+      document.getElementById("name").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+    }// función limpiarformulario
+
+    function enviarFormulario(event) {
       event.preventDefault(); // Prevenir el envío del formulario por defecto
 
       if (validateForm()) {
@@ -63,17 +73,21 @@
         const message = document.getElementById("message").value;
 
         // Enviar el correo electrónico usando EmailJS
-        emailjs.send("service_9ch8skf", "template_ueznlx6", {
+          emailjs.send("service_xlnbxdg", "template_vr8df8f", {
           from_name: name,
           from_phone: phone,
           from_email: email,
           message: message
         }).then(function(response) {
-          alert("Correo enviado exitosamente!");
-        }, function(error) {
-          alert("Hubo un error al enviar el correo: " + JSON.stringify(error));
+          alert("Mensaje enviado exitosamente.") 
+          limpiarFormulario();
         });
-      }
-    }
+      } else {console.log("datos invalidos")};
+    }// función enviarFormulario
 
-     */
+    let alertMessage = document.getElementById("alertMessage").innerHTML
+
+      document.getElementById("contactForm").addEventListener("submit", function(event) {
+      validateForm(); // Validar formulario antes de enviar
+      enviarFormulario(event); // Enviar formulario si es válido
+    }); //evento submit botón 
