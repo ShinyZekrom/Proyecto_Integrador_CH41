9dd33390d7fed33978da3600e65a20c3297301d2
Zekrom <maldonado.luis.iqm@gmail.com>
2024-06-24 17:19:21 -0600

    Se verifica responsividad de la página, se acomoda de acuerdo  al diseño de figma y se añaden comentarios sobre el funcionamiento del login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     // Almacenar usuario de prueba en localStorage
     const usuarioPrueba = {
         email: 'delhazsocialmedia@gmail.com',
         password: 'Pa$$w0rd'
     };
+    // Almacenar usuario como JSON
     localStorage.setItem('usuarioPrueba', JSON.stringify(usuarioPrueba));
 
     const submitBtn = document.getElementById('submit-btn');
@@ -13,32 +13,42 @@ document.addEventListener('DOMContentLoaded', function() {
     const alertValidaciones = document.getElementById('alertValidaciones');
     const alertValidacionesTexto = document.getElementById('alertValidacionesTexto');
 
+    // Función del botón para validar compos e iniciar sesión
     submitBtn.addEventListener('click', function(e) {
         e.preventDefault();
         const email = document.getElementById('email').value;
         const password = document.getElementById('inputPassword').value;
 
-        // Validar campos vacíos
+        // Validar campos vacíos 
         if (!email || !password) {
             showAlert('Por favor, complete todos los campos.', 'danger');
             return;
         }
 
-        // Obtener usuario del localStorage
+        // Obtener usuarios del localStorage
         const usuarioGuardado = JSON.parse(localStorage.getItem('usuarioPrueba'));
+        const formRegistro = JSON.parse(localStorage.getItem('formRegistro'));
 
-        // Validar credenciales
+        // Validar usuarios, se utiliza un else if para poder validar tanto la prueba de un usuario como el obtenido en el form registro
         if (email === usuarioGuardado.email && password === usuarioGuardado.password) {
-            showAlert('¡Inicio de sesión exitoso!', 'success');
-            // Redirigir a la página de inicio después de 3 segundos
-            setTimeout(() => {
-                window.location.href = 'paginaPrincipal.html'; //Ruta correcta a tu página de inicio
-            }, 3000);
+            loginExitoso();
+        } else if (formRegistro && email === formRegistro.email && password === formRegistro.password) {
+            loginExitoso();
         } else {
             showAlert('Correo electrónico y/o contraseña incorrectos.', 'danger');
         }
     });
 
+    // Función para redirigir a la página de inicio 
+    function loginExitoso() {
+        showAlert('¡Inicio de sesión exitoso!', 'success');
+        // Redirigir a la página de inicio después de 3 segundos
+        setTimeout(() => {
+            window.location.href = 'paginaPrincipal.html'; //Ruta correcta a tu página de inicio
+        }, 3000);
+    }
+
+    // Función para mostrar alertas
     function showAlert(message, type) {
         errorMessage.classList.add('d-none');
         alertSuccess.style.display = 'none';
