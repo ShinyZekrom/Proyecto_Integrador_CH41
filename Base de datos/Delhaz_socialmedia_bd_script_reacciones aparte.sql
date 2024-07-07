7bc7c3b0da0f88327ba26b69ab50a6cd2cd99e38
Zekrom <maldonado.luis.iqm@gmail.com>
2024-07-07 01:09:31 -0600

    Se realiza la modificación de la bd para que unicamente se llame delhaz

--- Base de datos/Delhaz_socialmedia_bd_script_reacciones aparte.sql
+++ Base de datos/Delhaz_socialmedia_bd_script_reacciones aparte.sql
@@ -5,6 +5,13 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema delhaz
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
 -- Schema delhaz
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `delhaz` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
@@ -14,100 +21,103 @@ USE `delhaz` ;
 -- Table `delhaz`.`usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `delhaz`.`usuarios` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(80) NOT NULL,
-  `username` VARCHAR(15) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `contraseña` VARCHAR(255) NOT NULL,
-  `fecha_registro` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `fecha_registro` DATETIME(6) NOT NULL,
   `foto_perfil` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `username` (`username` ASC) VISIBLE
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+  `nombre` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `username` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
 
 -- -----------------------------------------------------
 -- Table `delhaz`.`perfiles`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `delhaz`.`perfiles` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `usuario_id` INT NOT NULL,
-  `biografia` TEXT NULL DEFAULT NULL,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `descripcion` TEXT NULL DEFAULT NULL,
+  `usuarios_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `usuario_id` (`usuario_id` ASC) VISIBLE,
-  CONSTRAINT `perfiles_ibfk_1`
-    FOREIGN KEY (`usuario_id`)
+  INDEX `fk_perfiles_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_perfiles_usuarios1`
+    FOREIGN KEY (`usuarios_id`)
     REFERENCES `delhaz`.`usuarios` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
 
 -- -----------------------------------------------------
--- Table `delhaz`.`publicaciones`
+-- Table `delhaz`.`publicacion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `delhaz`.`publicaciones` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `usuario_id` INT NOT NULL,
-  `descripcion` TEXT NOT NULL,
-  `fecha_publicacion` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `contenido` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `usuario_id` (`usuario_id` ASC) VISIBLE,
-  CONSTRAINT `publicaciones_ibfk_1`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `delhaz`.`usuarios` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `delhaz`.`publicacion` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `contenido` VARCHAR(255) NOT NULL,
+  `descripcion` VARCHAR(255) NOT NULL,
+  `fecha_publicacion` DATETIME(6) NOT NULL,
+  `usuario` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
 
 -- -----------------------------------------------------
 -- Table `delhaz`.`tipos_reacciones`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `delhaz`.`tipos_reacciones` (
-  `id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NOT NULL,
-  `icono` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `icono` VARCHAR(255) NULL DEFAULT NULL,
+  `nombre` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
 
 -- -----------------------------------------------------
 -- Table `delhaz`.`reacciones`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `delhaz`.`reacciones` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `usuario_id` INT NOT NULL,
-  `publicacion_id` INT NOT NULL,
-  `tipo_reaccion_id` TINYINT UNSIGNED NOT NULL,
-  `fecha_reaccion` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `fecha_reaccion` DATETIME(6) NOT NULL,
+  `usuarios_id` BIGINT NOT NULL,
+  `publicacion_id` BIGINT NOT NULL,
+  `tipos_reacciones_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_reaction` (`usuario_id`, `publicacion_id`),
-  INDEX `publicacion_id` (`publicacion_id` ASC) VISIBLE,
-  INDEX `tipo_reaccion_id` (`tipo_reaccion_id` ASC) VISIBLE,
-  CONSTRAINT `reacciones_ibfk_1`
-    FOREIGN KEY (`usuario_id`)
+  INDEX `fk_reacciones_usuarios_idx` (`usuarios_id` ASC) VISIBLE,
+  INDEX `fk_reacciones_publicacion1_idx` (`publicacion_id` ASC) VISIBLE,
+  INDEX `fk_reacciones_tipos_reacciones1_idx` (`tipos_reacciones_id` ASC) VISIBLE,
+  CONSTRAINT `fk_reacciones_usuarios`
+    FOREIGN KEY (`usuarios_id`)
     REFERENCES `delhaz`.`usuarios` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `reacciones_ibfk_2`
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_reacciones_publicacion1`
     FOREIGN KEY (`publicacion_id`)
-    REFERENCES `delhaz`.`publicaciones` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `reacciones_ibfk_3`
-    FOREIGN KEY (`tipo_reaccion_id`)
+    REFERENCES `delhaz`.`publicacion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_reacciones_tipos_reacciones1`
+    FOREIGN KEY (`tipos_reacciones_id`)
     REFERENCES `delhaz`.`tipos_reacciones` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- Insertar tipos de reacciones
-INSERT INTO `delhaz`.`tipos_reacciones` (nombre, icono) VALUES
-('Me encanta', '/path/to/love_icon.svg'),
-('Me inspira', '/path/to/inspire_icon.svg'),
-('Me divierte', '/path/to/laugh_icon.svg'),
-('Me interesa', '/path/to/interest_icon.svg'),
-('Me entristece', '/path/to/sad_icon.svg');
-
