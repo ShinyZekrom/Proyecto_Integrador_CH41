12eb5f5778b835f2c2f3c51544a13fc0066dfd08
Carol <carol.hernandezc1999@gmail.com>
2024-07-10 23:44:16 -0600

    cambios de form pulicaciones y login

--- javascript/formPublicacion.js
+++ javascript/formPublicacion.js
@@ -43,6 +43,8 @@ document.addEventListener("DOMContentLoaded", function() {
       alertValidacionesTexto.innerHTML = errors.join('<br>');
       alertSuccess.style.display = 'none';
     } else {
+
+
       // Crear objeto con la información de la publicación
       const nuevaPublicacion = {
         id: Date.now(),
@@ -64,6 +66,51 @@ document.addEventListener("DOMContentLoaded", function() {
       
       console.log("Nueva publicación añadida:", nuevaPublicacion);
       console.log("Total de publicaciones:", publicaciones.length);
+
+      //////////////////////////////////////////////
+
+    // Enviar la publicación al servidor
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = JSON.stringify({
+        "usuario": loggedInUser.username,
+        "descripcion": description,
+        "fechaPublicacion": nuevaPublicacion.timestamp,
+        "contenido": img
+    });
+    
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+   
+    fetch("http://localhost:8080/api/login/", requestOptions)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Error al enviar la publicación');
+    }
+    return response.json(); // Cambiar a .json() si el servidor devuelve JSON
+  })
+  .then((result) => {
+    console.log("Respuesta del servidor:", result);
+
+    // Guardar la información del usuario en localStorage
+    localStorage.setItem('loggedInUser', JSON.stringify(result));
+
+    // Redirigir a la página principal
+    window.location.href = 'paginaPrincipal.html';
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+
+  });
+
+
+  ///////////////////Fin Fetch 
 
       // Mostrar mensaje de éxito
       alertSuccess.style.display = 'block';
